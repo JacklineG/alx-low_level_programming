a2efe78c0aa62e7c788bf40705247c3dfa2cc5bc
JacklineG <jakyshe@gmail.com>
2023-02-17 03:43:49 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc printf (" with proper grammar, but the outcome is a piece of art")
+#include<stdio.h>
+/**
+ * main - using printf function to print
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
