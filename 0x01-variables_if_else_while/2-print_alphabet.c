69233b0d7d198c60126fab09a8600afd74d93f41
JacklineG <jakyshe@gmail.com>
2023-02-17 18:43:37 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-ankj
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
