29e3f56b7c70f6003473a16bad742df74cdaa61f
JacklineG <jakyshe@gmail.com>
2023-02-19 15:40:40 -0800

    Tak 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - write aphabets in lower case except e and q
+ * Return:0 always a success
+ */
+int main(void)
+{
+	char small q, e;
+
+	e = 'e';
+	q = 'q';
+
+	for (small = 'a'; small <= 'z'; small ++)
+	{
+		if (small != e && small != q)
+			putchar(small);
+	}
+	putchar('\n');
+	return (0);
+}
