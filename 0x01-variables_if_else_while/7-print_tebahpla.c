cc85b01eecd9b7a4cf4f3504df898882b835a6e5
JacklineG <jakyshe@gmail.com>
2023-02-19 16:37:53 -0800

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <tim.h>
 
 /**
- * main - lowercase alphabet in reverse
- * return:0 always
-*/
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
 
 int main(void)
 {
