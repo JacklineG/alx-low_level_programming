54176489c95fb382364944ed69c4f27e10c96c9a
JacklineG <jakyshe@gmail.com>
2023-03-08 16:53:30 -0800

    task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
-i
 #include "main.h"
 /**
  * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ *  @s: input;
+ * Return: Always 0 (Success);
  */
 void _puts_recursion(char *s)
 {
